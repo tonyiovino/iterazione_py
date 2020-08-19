8b1df23d00fef84e03a07b02d821ff42783f6f05
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-19 19:59:31 +0200

    Liste

---
+++ lista.py
@@ -0,0 +1,8 @@
+lista = ["\nXDJ-RX2:", 1680, "\nDDJ-SB2:", 150, "\nDDJ-SB3:", 280]
+
+print("La lista è:", lista)
+
+print("\nI prezzi delle console sono:")
+
+for console in lista:
+    print(console)
