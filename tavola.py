f36f0cce8ac99e0868270bca898cb54a5ac49aa8
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-19 21:06:16 +0200

    Liste

---
+++ tavola.py
@@ -0,0 +1,13 @@
+colonna = 1
+while colonna <= 10:
+    riga = 1
+
+    while riga <= 10:
+        prodotto = riga * colonna
+
+        print(prodotto)
+
+        riga+=1
+
+    print("\n")
+    colonna+=1
