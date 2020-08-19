375ff3f69a7bfcd532175abd82819df340457af9
tonyiovino <antonioiovino2004tony@gmail.com>
2020-08-19 21:57:38 +0200

    Pi_Greco

--- pi_greco.py
+++ pi_greco.py
@@ -3,11 +3,11 @@ num = int(num)
 
 divisore = 1
 count = 0
-pi_greco = 0
+pi_greco = 0.
 segno = 1
 
 while count < num:
-    pi_greco = pi_greco + segno * 4/divisore
+    pi_greco = pi_greco + segno * 4./divisore
     segno = - segno
     count+=1
     divisore+=2
